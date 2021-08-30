fbf304cd67306bc18f0cedae1c99604cd7584fad
João Vítor Carvalho <mistingerr@gmail.com>
2021-08-30 12:08:28 -0300

    fetch hook deployed

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Footer from './Components/Footer';
 import Home from './Components/Home';
 import Login from './Components/Login/Login';
 import { UserStorage } from './UserContext';
+import User from './Components/User/User';
 
 
 
@@ -17,7 +18,8 @@ function App() {
           <Header />
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/login/*" element={<Login />} />
+            <Route path="login/*" element={<Login />} />
+            <Route path="conta/*" element={<User />} />
           </Routes>
           <Footer />
         </UserStorage>
