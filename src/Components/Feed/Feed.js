5786f0361b130c7defdcf0d48874158323ce6f1c
João Vítor Carvalho <mistingerr@gmail.com>
2021-08-31 16:23:57 -0300

    infinite-scroll-applied

--- src/Components/Feed/Feed.js
+++ src/Components/Feed/Feed.js
@@ -3,11 +3,47 @@ import FeedModal from './FeedModal'
 import FeedPhotos from './FeedPhotos'
 
 const Feed = ({ user }) => {
-  const [modalPhoto, setModalPhoto] = React.useState(null)
+  const [modalPhoto, setModalPhoto] = React.useState(null);
+  const [pages, setPages] = React.useState([1, 2]);
+  const [infinite, setInfinite] = React.useState(true);
+
+  React.useEffect(() => {
+    let wait = false;
+    function infiniteScroll() {
+     if (infinite) { const scroll = window.scrollY;
+      const height = document.body.offsetHeight - window.innerHeight;
+        if(scroll > height * .75 && !wait) {
+          setPages((pages) => [...pages, pages.length + 1]);
+          wait = true;
+          setTimeout(()=> {
+            wait = false;
+          }, 500)
+        }
+      }
+    }
+
+    window.addEventListener('scroll', infiniteScroll);
+    window.addEventListener('wheel', infiniteScroll);
+    return () => {
+      window.removeEventListener('scroll', infiniteScroll);
+    window.removeEventListener('wheel', infiniteScroll);
+    };
+  }, [infinite])
+
   return (
     <div>
-      {modalPhoto && <FeedModal photo={modalPhoto} setModalPhoto={setModalPhoto} />}
-      <FeedPhotos setModalPhoto={setModalPhoto} />
+      {modalPhoto && (
+        <FeedModal photo={modalPhoto} setModalPhoto={setModalPhoto} />
+      )}
+      {pages.map(page => (
+        <FeedPhotos 
+          key={page} 
+          user={user} 
+          page={page} 
+          setInfinite={setInfinite}
+          setModalPhoto={setModalPhoto} 
+          />
+      ))}
     </div>
   )
 }
