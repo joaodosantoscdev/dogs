f3cdd4ac64f65452652292843bfb29037eb63ffc
João Vítor Carvalho <mistingerr@gmail.com>
2021-08-31 18:25:50 -0300

    user perfil developed, added some archives for the login reset

--- src/Components/Feed/Feed.js
+++ src/Components/Feed/Feed.js
@@ -1,6 +1,7 @@
-import React from 'react'
-import FeedModal from './FeedModal'
-import FeedPhotos from './FeedPhotos'
+import React from 'react';
+import FeedModal from './FeedModal';
+import FeedPhotos from './FeedPhotos';
+import PropTypes from 'prop-types';
 
 const Feed = ({ user }) => {
   const [modalPhoto, setModalPhoto] = React.useState(null);
@@ -45,7 +46,18 @@ const Feed = ({ user }) => {
           />
       ))}
     </div>
-  )
+  );
+};
+
+Feed.defaultProps = {
+  user: 0,
+};
+
+Feed.ReactPropTypes = {
+  user: PropTypes.oneOfType([
+    PropTypes.string.isRequired, 
+    PropTypes.number.isRequired
+  ]),
 }
 
 export default Feed
--- src/Components/Login/LoginPasswordReset.js
+++ src/Components/Login/LoginPasswordReset.js
@@ -1,6 +1,12 @@
 import React from 'react'
 
 const LoginPasswordReset = () => {
+  const [login, setLogin] = React.useState('');
+  const [key, setKey] = React.useState('');
+
+  React.useEffect(() => {
+
+  }, [])
   return (
     <div>
       Reset
