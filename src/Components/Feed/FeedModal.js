379b05bb141ba15db121f5a31c3e9fa3547b0606
João Vítor Carvalho <mistingerr@gmail.com>
2021-08-31 19:40:59 -0300

    archives ajusted

--- src/Components/Feed/FeedModal.js
+++ src/Components/Feed/FeedModal.js
@@ -5,7 +5,7 @@ import styles from './FeedModal.module.css'
 import Error from '../Helper/Error';
 import Loading from '../Helper/Loading';
 import PhotoContent from '../Photo/PhotoContent';
-import Head from '../Helper/Head';
+
 
 
 const FeedModal = ({ photo, setModalPhoto }) => {
